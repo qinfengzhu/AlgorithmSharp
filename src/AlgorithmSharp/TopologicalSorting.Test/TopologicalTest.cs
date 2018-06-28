4a69540d3a8540b9171bcb6902f4a3e61efbfa79
qinfeng.zhu <qinfeng.zhu@ctsmed.com>
2018-06-28 17:45:25 +0800

    [add] some files

---
+++ src/AlgorithmSharp/TopologicalSorting.Test/TopologicalTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TopologicalSorting.Test
+{
+    [TestFixture]
+    public class TopologicalTest
+    {
+        [Test]
+        public void BoolPraseTest()
+        {
+            bool actived = true;
+            bool.TryParse(string.Empty, out actived);
+            Assert.AreEqual(false,actived);
+            bool.TryParse(null, out actived);
+            Assert.AreEqual(false, actived);
+        }
+    }
+}
